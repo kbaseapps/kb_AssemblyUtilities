9a39c25aa2da275f04028944bf3a21f6fe77b60b
dcchivian <dcchivian@lbl.gov>
2020-04-07 00:42:09 +0000

    finished initial setup

--- lib/kb_AssemblyUtilities/kb_AssemblyUtilitiesImpl.py
+++ lib/kb_AssemblyUtilities/kb_AssemblyUtilitiesImpl.py
@@ -22,9 +22,9 @@ class kb_AssemblyUtilities:
     # state. A method could easily clobber the state set by another while
     # the latter method is running.
     ######################################### noqa
-    VERSION = "0.0.1"
-    GIT_URL = ""
-    GIT_COMMIT_HASH = ""
+    VERSION = "1.0.0"
+    GIT_URL = "https://github.com/dcchivian/kb_AssemblyUtilities"
+    GIT_COMMIT_HASH = "24be0d5da2a4d343f44ad3610841ce67eb9687f0"
 
     #BEGIN_CLASS_HEADER
     #END_CLASS_HEADER
@@ -41,32 +41,67 @@ class kb_AssemblyUtilities:
         pass
 
 
-    def run_kb_AssemblyUtilities(self, ctx, params):
+    def run_filter_contigs_by_length(self, ctx, params):
         """
-        This example function accepts any number of parameters and returns results in a KBaseReport
-        :param params: instance of mapping from String to unspecified object
+        :param params: instance of type "Filter_Contigs_by_Length_Params"
+           (filter_contigs_by_length() ** **  Remove Contigs that are under a
+           minimum threshold) -> structure: parameter "workspace_name" of
+           type "workspace_name" (** The workspace object refs are of form:
+           ** **    objects = ws.get_objects([{'ref':
+           params['workspace_id']+'/'+params['obj_name']}]) ** ** "ref" means
+           the entire name combining the workspace id and the object name **
+           "id" is a numerical identifier of the workspace or object, and
+           should just be used for workspace ** "name" is a string identifier
+           of a workspace or object.  This is received from Narrative.),
+           parameter "input_assembly_refs" of type "data_obj_ref", parameter
+           "min_contig_length" of Long, parameter "output_name" of type
+           "data_obj_name"
         :returns: instance of type "ReportResults" -> structure: parameter
            "report_name" of String, parameter "report_ref" of String
         """
         # ctx is the context object
-        # return variables are: output
-        #BEGIN run_kb_AssemblyUtilities
-        report = KBaseReport(self.callback_url)
-        report_info = report.create({'report': {'objects_created':[],
-                                                'text_message': params['parameter_1']},
-                                                'workspace_name': params['workspace_name']})
-        output = {
-            'report_name': report_info['name'],
-            'report_ref': report_info['ref'],
-        }
-        #END run_kb_AssemblyUtilities
+        # return variables are: returnVal
+        #BEGIN run_filter_contigs_by_length
+        #END run_filter_contigs_by_length
 
         # At some point might do deeper type checking...
-        if not isinstance(output, dict):
-            raise ValueError('Method run_kb_AssemblyUtilities return value ' +
-                             'output is not type dict as required.')
+        if not isinstance(returnVal, dict):
+            raise ValueError('Method run_filter_contigs_by_length return value ' +
+                             'returnVal is not type dict as required.')
         # return the results
-        return [output]
+        return [returnVal]
+
+    def run_fractionate_contigs(self, ctx, params):
+        """
+        :param params: instance of type "Fractionate_Contigs_Params"
+           (fractionate_contigs() ** **  Split Assemblies into
+           Presence/Absence with respect to other objects) -> structure:
+           parameter "workspace_name" of type "workspace_name" (** The
+           workspace object refs are of form: ** **    objects =
+           ws.get_objects([{'ref':
+           params['workspace_id']+'/'+params['obj_name']}]) ** ** "ref" means
+           the entire name combining the workspace id and the object name **
+           "id" is a numerical identifier of the workspace or object, and
+           should just be used for workspace ** "name" is a string identifier
+           of a workspace or object.  This is received from Narrative.),
+           parameter "input_assembly_ref" of type "data_obj_ref", parameter
+           "input_pos_filter_obj_refs" of type "data_obj_name", parameter
+           "output_name" of type "data_obj_name", parameter
+           "fractionate_mode" of String
+        :returns: instance of type "ReportResults" -> structure: parameter
+           "report_name" of String, parameter "report_ref" of String
+        """
+        # ctx is the context object
+        # return variables are: returnVal
+        #BEGIN run_fractionate_contigs
+        #END run_fractionate_contigs
+
+        # At some point might do deeper type checking...
+        if not isinstance(returnVal, dict):
+            raise ValueError('Method run_fractionate_contigs return value ' +
+                             'returnVal is not type dict as required.')
+        # return the results
+        return [returnVal]
     def status(self, ctx):
         #BEGIN_STATUS
         returnVal = {'state': "OK",
--- test/kb_AssemblyUtilities_server_test.py
+++ test/kb_AssemblyUtilities_server_test.py
@@ -9,7 +9,10 @@ from kb_AssemblyUtilities.kb_AssemblyUtilitiesServer import MethodContext
 from kb_AssemblyUtilities.authclient import KBaseAuth as _KBaseAuth
 
 from installed_clients.WorkspaceClient import Workspace
-
+from installed_clients.AssemblyUtilClient import AssemblyUtil
+from installed_clients.GenomeFileUtilClient import GenomeFileUtil
+from installed_clients.MetagenomeUtilsClient import MetagenomeUtils
+from installed_clients.SetAPIServiceClient import SetAPI
 
 class kb_AssemblyUtilitiesTest(unittest.TestCase):
 
@@ -52,16 +55,53 @@ class kb_AssemblyUtilitiesTest(unittest.TestCase):
             cls.wsClient.delete_workspace({'workspace': cls.wsName})
             print('Test workspace was deleted')
 
+
+    ##############
+    # UNIT TESTS #
+    ##############
+
     # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
-    def test_your_method(self):
-        # Prepare test objects in workspace if needed using
-        # self.getWsClient().save_objects({'workspace': self.getWsName(),
-        #                                  'objects': []})
-        #
-        # Run your method by
-        # ret = self.getImpl().your_method(self.getContext(), parameters...)
-        #
-        # Check returned data with
-        # self.assertEqual(ret[...], ...) or other unittest methods
-        ret = self.serviceImpl.run_kb_AssemblyUtilities(self.ctx, {'workspace_name': self.wsName,
-                                                             'parameter_1': 'Hello World!'})
+
+    #### test_filter_contigs_by_length_01()
+    ##
+    def test_filter_contigs_by_length_01 (self):
+        method = 'filter_contigs_by_length_01'
+        
+        print ("\n\nRUNNING: test_filter_contigs_by_length_01()")
+        print ("===========================================\n\n")
+
+        # upload test data
+        try:
+            auClient = AssemblyUtil(self.callback_url, token=self.getContext()['token'])
+        except Exception as e:
+            raise ValueError('Unable to instantiate auClient with callbackURL: '+ self.callback_url +' ERROR: ' + str(e))
+        ass_file_1 = 'assembly_1.fa'
+        ass_file_2 = 'assembly_2.fa'
+        ass_path_1 = os.path.join(self.scratch, ass_file_1)
+        ass_path_2 = os.path.join(self.scratch, ass_file_2)
+        shutil.copy(os.path.join("data", ass_file_1), ass_path_1)
+        shutil.copy(os.path.join("data", ass_file_2), ass_path_2)
+        ass_ref_1 = auClient.save_assembly_from_fasta({
+            'file': {'path': ass_path_1},
+            'workspace_name': self.getWsName(),
+            'assembly_name': 'assembly_1'
+        })
+        ass_ref_2 = auClient.save_assembly_from_fasta({
+            'file': {'path': ass_path_2},
+            'workspace_name': self.getWsName(),
+            'assembly_name': 'assembly_2'
+        })
+
+        # run method
+        input_refs = [ ass_ref_1, ass_ref_2 ]
+        base_output_name = method+'_output'
+        params = {
+            'workspace_name': self.getWsName(),
+            'input_assembly_refs': input_refs,
+            'min_contig_length': 1000,
+            'output_name': 'test_filtered'
+        }
+        result = self.getImpl().run_filter_contigs_by_length(self.getContext(),params)
+        print('RESULT:')
+        pprint(result)
+        pass
